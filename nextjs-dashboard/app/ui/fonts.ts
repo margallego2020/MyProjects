8b51309dafa46d75ee3a5b618ad768f6e4eef95c
margallego2020 <martingallego_ar@yahoo.com.ar>
2023-11-03 10:29:23 -0300

    Chapter 4 completed

---
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,13 @@
+import { Inter, Lusitana, Raleway } from "next/font/google";
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+    weight: ["400"],
+    subsets:['latin']
+});
+
+export const raleway = Raleway({
+    weight: ["500"],
+    subsets: ['vietnamese'],
+})
