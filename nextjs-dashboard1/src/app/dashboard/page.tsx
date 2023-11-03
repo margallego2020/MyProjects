6543e2c4e553759f3ddd600b1c1e564e1f9ee00a
margallego2020 <martingallego_ar@yahoo.com.ar>
2023-11-03 12:12:41 -0300

    Volviendo al original. Lo 2do no me salió

--- nextjs-dashboard1/src/app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Card } from "@/ui/dashboard/cards"
-import LatestInvoices from "@/ui/dashboard/latest-invoices"
-import RevenueChart from "@/ui/dashboard/revenue-chart"
-import { Lusitana } from "next/font/google"
-
-
-export default function Page() {
-    return <p>Dashboard Page</p>
-}
