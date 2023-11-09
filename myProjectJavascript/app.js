f8f4efeba35fbafedcdad92feba6ec6863065b89
margallego2020 <martingallego_ar@yahoo.com.ar>
2023-11-09 19:44:38 -0300

    Empezando

--- myProjectJavascript/app.js
+++ myProjectJavascript/app.js
@@ -1,3 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send("Hola Martin!!!!!");
+});
+
+app.listen(3000, () => console.log("Servidor corriendo en http://localhost:3000"));
