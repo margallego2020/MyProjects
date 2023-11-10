18d59339d23e47697682a6ecdc137413d4e36c46
margallego2020 <martingallego_ar@yahoo.com.ar>
2023-11-10 09:44:49 -0300

    Probando cosas 3

--- myProjectJavascript/app.js
+++ myProjectJavascript/app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+// Configuro ejs como el motor del template engine de la app
+app.set("view engine", "ejs");
+// Defino la ubicación de la carpeta de las vistas
+app.set("views", path.join(__dirname, "./views"));
+// Configuro acceso a la carpeta de recursos estáticos 
+app.use(express.static(path.resolve(__dirname, "../public")));
 
 app.get("/", (req, res) => {
-    res.send("Hola Martin!!!!!");
+    // res.send("Hola Martin!!!!!");
+    res.render("../src/views/index");
 });
 
 app.listen(3000, () => console.log("Servidor corriendo en http://localhost:3000"));
